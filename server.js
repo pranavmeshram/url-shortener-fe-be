2bb8114ec72f887187b4edb69020eb2f98f82364
pranavmeshram <pranav.sk.meshram@gmail.com>
2025-07-28 14:12:32 +0530

    adding routes, controllers and complete UI

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import { createNewShortUrl, getLongUrl } from './controllers/url.js';
 
 const app = express();
 const port = 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(path.resolve(), 'public')));
+
+// Db connection
+mongoose
+    .connect('mongodb://127.0.0.1:27017/wdmShortUrl')
+    .then(() => console.log('\nMongoDb connected\n'))
+    .catch((err) => console.log('MongoDb Error', err));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.post('/', createNewShortUrl);
+
+app.get('/:shortId', getLongUrl);
 
 app.listen(port, () => {
     console.log(`url-shortener app listening on port ${port}`);
