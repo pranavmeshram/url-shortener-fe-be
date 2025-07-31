485be287a0da7d8d62fe8128a3c27fcd0a551576
pranavmeshram <pranav.sk.meshram@gmail.com>
2025-07-31 12:33:21 +0530

    adding controller and business logic

--- controllers/url.js
+++ controllers/url.js
@@ -1,3 +1,35 @@
-export const createNewShortUrl = async (req, res) => {};
+import { Url } from '../models/url.js';
+import { newRandomString } from '../utils/stringGenerator.js';
 
-export const getLongUrl = async (req, res) => {};
+export const createNewShortUrl = async (req, res) => {
+    const longUrl = req.body.longUrl;
+    if (!longUrl) throw new Error('Url not found!');
+    try {
+        const payload = {
+            shortId: newRandomString,
+            longUrl: longUrl,
+        };
+        await Url.create(payload);
+        res.render('index.ejs', { shortId: newRandomString });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong !',
+            result: error.message,
+        });
+    }
+};
+
+export const getLongUrl = async (req, res) => {
+    try {
+        const result = await Url.findOne({ shortId: req.params.shortId });
+        if (!result) throw new Error('Url not found!');
+        res.redirect(result.longUrl);
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong !',
+            result: error.message,
+        });
+    }
+};
